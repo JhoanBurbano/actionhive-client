665836e4e4d87a695c0cb0343ff95a82f8b75d02
jsburbano <joans.burbano@gmail.com>
2024-10-15 15:51:45 -0500

    feat: ad list

--- src/components/Organisms/ProjectsViewer.tsx
+++ src/components/Organisms/ProjectsViewer.tsx
@@ -28,6 +28,7 @@ const ProjectsViewer: React.FC<ProjectsViewerProps> = ({ projects }) => {
       spaceBetween: 10,
     },
   };
+  const [showList, setShowList] = useState(false);
 
   const goToDetails = (id: string) => {
     dispatch(thunkGetProject(id));
@@ -43,6 +44,7 @@ const ProjectsViewer: React.FC<ProjectsViewerProps> = ({ projects }) => {
       {projects.length ? (
         <>
           <FilterList setSearch={setSearch} search={search}/>
+          <button onClick={() => setShowList(true)} className="w-20 h-10 text-xs bg-green-600 rounded-md hover:bg-green-900 text-white ">Mostrar lista</button>
           <Swiper
             breakpoints={breakpoints}
             pagination={{
@@ -117,6 +119,65 @@ const ProjectsViewer: React.FC<ProjectsViewerProps> = ({ projects }) => {
       ) : (
         <section className="dashboard__panel-text">No tienes proyectos</section>
       )}
+      {
+        (projects.length && showList) && (
+          <div className="fixed top-0 left-0 z-[10000000] overflow-y-auto bg-[#000a] w-full p-10 h-full">
+            <main className="bg-white p-4 rounded-lg flex flex-col">
+            <button onClick={() => setShowList(false)} className="w-20 h-10 text-xs bg-red-600 rounded-md hover:bg-red-900 text-white ">Ocultar lista</button>
+            <FilterList setSearch={setSearch} search={search}/>
+            <section className="">
+              <ul>
+                {searchedProjects.map(
+                  (
+                    {
+                      id,
+                      projectName,
+                      description,
+                      developmentStatus,
+                      fundingCap,
+                    },
+                    i
+                  ) => (
+                    <>
+                    <li key={i} className="flex items-center">
+                      <p className="font-bold">{i+1}.</p>
+                      <article
+                        className="bg-white p-4 flex flex-col gap-4 cursor-pointer hover:bg-green-100 transition-colors"
+                        onClick={() => goToDetails(id)}
+                      >
+                        <h3 className="font-bold underline">
+                            {projectName}
+                          </h3>
+                          <p className="italic text-zinc-400" >
+                            {description}
+                          </p>
+                          <span>
+                          <p className="text-xs">Progreso</p>
+                          <span className="progress h-2">
+                              <span
+                                className="progress-bar"
+                                style={{ width: developmentStatus! * 100 + "%" }}
+                              ></span>
+                            </span>
+                          </span>
+                          <span className="">
+                            <h3 className="text-xs">Objetivo financiero</h3>
+                            <p className="text-xs">
+                              COP{" "}
+                              <b className="text-sm">{fundingCap!.toString().replace(CURRENCY_REGEX, ".")}</b>
+                            </p>
+                          </span>
+                      </article>
+                    </li>
+                    </>
+                  )
+                )}
+              </ul>
+            </section>
+            </main>
+          </div>
+        )
+      }
     </>
   );
 };
